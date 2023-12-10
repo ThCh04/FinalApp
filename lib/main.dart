02df78833334bb936ad01e8fbd2c09978c095b65
Eduard Cruz <cc.eduardantonio4@gmail.com>
2023-12-10 19:18:08 -0400

    FinalApp: Configuracion de los servicios de Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:finalapp/pages/login_page.dart';
-import 'package:finalapp/routes.dart';
+import 'package:finalapp/services/firebase/firebase_service.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await initFirebase();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: const LoginView(),
-      routes: getRoutes(context),
-    );
+    return const LoginView();
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -6,7 +6,11 @@ import 'package:flutter/material.dart';
 Map<String, WidgetBuilder> getRoutes(BuildContext context){
   Map<String, WidgetBuilder> routes = {
     "/login" : (context) => const LoginView(),
+<<<<<<< HEAD
     "/news" : (context) => NewsScreen(),
+=======
+
+>>>>>>> parent of a622f17 (Mapas y multas)
   };
 
   return routes;
