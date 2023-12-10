0152fee34b8684070c687251f9671e9db5b4245a
Eduard Cruz <cc.eduardantonio4@gmail.com>
2023-12-10 19:19:26 -0400

    Revert "FinalApp: Configuracion de los servicios de Firebase"
    This reverts commit 02df78833334bb936ad01e8fbd2c09978c095b65.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,62 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:finalapp/routes.dart';
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController userController = TextEditingController();
-    TextEditingController passController = TextEditingController();
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: getRoutes(context),
-      title: 'Login',
+      title: 'Material App',
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             const Padding(padding: EdgeInsets.all(15)),
-             const Text('Inicia Sesión', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),),
-             const Padding(padding: EdgeInsets.all(10)),
-             Container(
-               width: MediaQuery.of(context).size.width*0.8,
-               child: Column(
-                 children: [
-                   TextField(
-                     controller: userController,
-                     decoration: InputDecoration(
-                       hintText: 'Usuario', labelText: 'Usuario',
-                       border: OutlineInputBorder(
-                         borderRadius: BorderRadius.circular(10.0),
-                       ),
-                     ),
-                   ),
-                   const Padding(padding: EdgeInsets.all(5)),
-                   TextField(
-                     obscureText: true,
-                     controller: passController,
-                     decoration:  InputDecoration(
-                       hintText: 'Contraseña', labelText: 'Contraseña',
-                       border: OutlineInputBorder(
-                         borderRadius: BorderRadius.circular(10.0),
-                       ),
-                     ),
-                   ),
-                 ],
-               ),
-             ),
-             const Padding(padding: EdgeInsets.all(10)),
-             Row(
-               mainAxisAlignment: MainAxisAlignment.center,
-                 children: [
-                   Container(
-                     width: MediaQuery.of(context).size.width*0.8,
-                     child: Expanded(
-                       child: ElevatedButton(onPressed: (){}, child: const Text('Entrar'))),
-                   )]
-             )
-           ],
+          child: Container(
+            child: Text('Hello World'),
           ),
         ),
       ),
--- lib/routes.dart
+++ lib/routes.dart
@@ -6,11 +6,7 @@ import 'package:flutter/material.dart';
 Map<String, WidgetBuilder> getRoutes(BuildContext context){
   Map<String, WidgetBuilder> routes = {
     "/login" : (context) => const LoginView(),
-<<<<<<< HEAD
     "/news" : (context) => NewsScreen(),
-=======
-
->>>>>>> parent of a622f17 (Mapas y multas)
   };
 
   return routes;
