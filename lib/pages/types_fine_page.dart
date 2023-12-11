c1bf0311cd9886372dd26e273da06942400fb2f7
DwanilSanchez30 <dwanilsanchez01@gmail.com>
2023-12-11 15:48:05 -0400

    Ajustes a la pagina de tarifarios de multas
    Se añadió la conexión mediante la base de datos

--- lib/pages/types_fine_page.dart
+++ lib/pages/types_fine_page.dart
@@ -1,4 +1,3 @@
-// types_fine_page.dart
 import 'package:finalapp/widgets/mainDrawer_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:finalapp/services/types_fine_service.dart';
@@ -17,7 +16,17 @@ class _TypesFinePageState extends State<TypesFinePage> {
   @override
   void initState() {
     super.initState();
-    typesOfFines = _typesFineService.getTypesOfFines();
+    _loadTypesOfFines();
+  }
+
+  void _loadTypesOfFines() {
+    _typesFineService.getTypesOfFines().then((loadedTypesOfFines) {
+      setState(() {
+        typesOfFines = loadedTypesOfFines;
+      });
+    }).catchError((error) {
+      print('Error loading types of fines: $error');
+    });
   }
 
   @override
@@ -25,9 +34,13 @@ class _TypesFinePageState extends State<TypesFinePage> {
     return Scaffold(
       drawer: mainDrawer(context),
       appBar: AppBar(
-        title: const Text('Tipos de Multas de Tránsito'),
+        title: const Text('Tarifario de multas'),
       ),
-      body: ListView.builder(
+      body: typesOfFines == null
+          ? Center(
+        child: CircularProgressIndicator(),
+      )
+          : ListView.builder(
         itemCount: typesOfFines.length,
         itemBuilder: (context, index) {
           return TypeFineItem(typeFine: typesOfFines[index]);
@@ -44,10 +57,41 @@ class TypeFineItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(typeFine.name),
-      subtitle: Text(typeFine.description),
-      trailing: Text('\$${typeFine.amount.toString()}'),
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: ListTile(
+        title: Text(
+          typeFine.name,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: Colors.black87, // Cambia el color del texto del título
+          ),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 4),
+            Text(
+              typeFine.description,
+              style: TextStyle(
+                color: Colors.blue,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '\$${typeFine.amount.toString()}',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.red, // Cambia el color del texto del monto
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/services/types_fine_service.dart
+++ lib/services/types_fine_service.dart
@@ -1,20 +1,37 @@
-// types_fine_service.dart
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class TypeFine {
   String name;
   String description;
   double amount;
 
-  TypeFine({required this.name, required this.description, required this.amount});
+  TypeFine({
+    required this.name,
+    required this.description,
+    required this.amount,
+  });
 }
 
 class TypesFineService {
-  List<TypeFine> getTypesOfFines() {
-    // Simulamos un servicio que obtiene los tipos de multas desde una fuente externa, como una API
-    return [
-      TypeFine(name: 'Exceso de velocidad', description: 'Conducir a una velocidad superior al límite establecido.', amount: 100.0),
-      TypeFine(name: 'Estacionamiento prohibido', description: 'Estacionar en áreas no permitidas.', amount: 50.0),
-      // Agrega más tipos de multas según sea necesario
-    ];
+  final CollectionReference typeFinesCollection =
+  FirebaseFirestore.instance.collection('TypeFines');
+
+  Future<List<TypeFine>> getTypesOfFines() async {
+    try {
+      QuerySnapshot<Object?> snapshot =
+      await typeFinesCollection.get();
+
+      return snapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        return TypeFine(
+          name: data['name'] ?? '',
+          description: data['description'] ?? '',
+          amount: (data['amount'] ?? 0.0).toDouble(),
+        );
+      }).toList();
+    } catch (e) {
+      print('Error getting types of fines: $e');
+      return [];
+    }
   }
 }
